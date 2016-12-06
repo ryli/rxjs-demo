32537550814c4258b74da85342c7e879fd237c21
ELEVEN <lov.2014@gmail.com>
2016-12-06 16:46:26 +0800

    [chore] input event

--- src/index.js
+++ src/index.js
@@ -7,10 +7,18 @@ const app$_ = app$.subscribe()
 
 // ===== event =====
 
-const button = document.querySelector('button')
+const $button = document.querySelector('button')
 
-const button$ = Rx.Observable.fromEvent(button, 'click')
+const button$ = Rx.Observable.fromEvent($button, 'click')
 
 const button$_ = button$.subscribe(e => {
   console.log(e)
 })
+
+const $input = document.querySelector('input')
+
+const input$ = Rx.Observable.fromEvent($input, 'input')
+  .map(e => e.target.value.trim())
+  .do(val => console.log(val))
+
+const input$_ = input$.subscribe()
