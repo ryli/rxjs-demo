23173fbbc75f681e23905b5292892c7055c845d4
ELEVEN <lov.2014@gmail.com>
2016-12-06 16:39:51 +0800

    [chore] event

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import Rx from 'rxjs/Rx'
 const app$ = Rx.Observable.of(1,2,3,4,5)
   .do(r => console.log(r))
 
-app$.subscribe()
+const app$_ = app$.subscribe()
+
+// ===== event =====
+
+const button = document.querySelector('button')
+
+const button$ = Rx.Observable.fromEvent(button, 'click')
+
+const button$_ = button$.subscribe(e => {
+  console.log(e)
+})
