820d2ebc3643f08c39d0d703eeeae51be267c899
ELEVEN <lov.2014@gmail.com>
2016-12-06 17:37:12 +0800

    click event

--- src/index.js
+++ src/index.js
@@ -8,15 +8,18 @@ const app$_ = app$.subscribe()
 // ===== event =====
 
 const $button = document.querySelector('button')
-
-const button$ = Rx.Observable.fromEvent($button, 'click')
-
-const button$_ = button$.subscribe(e => {
-  console.log(e)
-})
-
 const $input = document.querySelector('input')
 const $result = document.querySelector('#result')
+
+const button$ = Rx.Observable.fromEvent($button, 'click')
+  .scan(count => count + 1, 0)
+  .do(val => {
+    console.log(val)
+    $result.innerHTML = `You clicked ${val} times`
+  })
+
+const button$_ = button$.subscribe()
+
 
 // problem: 不满足 filter 的条件后，result的状态还是之前的
 const input$ = Rx.Observable.fromEvent($input, 'input')
